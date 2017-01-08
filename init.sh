85897abfec02be1ac709558e4bb9e5ccd96b81a7
root <ktenman@users.noreply.github.com>
2017-01-08 23:17:30 +0200

    added db

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-docker cp $1:/src/db/cards.db /home/root;
+docker cp $1:/src/db/cards.db /home/root/computer-science-flash-cards;
 docker stop $1 && docker rm $1;
 docker rmi cs-flash-cards;
 docker build . -t cs-flash-cards;
-docker run -d -p 8000:8000 --name cs-flash-cards -v /home/root:/src/db cs-flash-cards;
+docker run -d -p 8000:8000 --name cs-flash-cards -v /home/root/computer-science-flash-cards:/src/db cs-flash-cards;
 docker volume rm $(docker volume ls -qf dangling=true);
