5179a0bcc227ddb7b2fd3e1a173f8d103c82b73f
root <ktenman@users.noreply.github.com>
2017-01-08 23:39:16 +0200

    added /root/computer-science-flash-cards into shell script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-docker cp $1:/src/db/cards.db /home/root/computer-science-flash-cards;
+docker cp $1:/src/db/cards.db `pwd`;
 docker stop $1 && docker rm $1;
 docker rmi cs-flash-cards;
 docker build . -t cs-flash-cards;
-docker run -d -p 8000:8000 --name cs-flash-cards -v /home/root/computer-science-flash-cards:/src/db cs-flash-cards;
+docker run -d -p 8000:8000 --name cs-flash-cards -v `pwd`:/src/db cs-flash-cards;
 docker volume rm $(docker volume ls -qf dangling=true);
