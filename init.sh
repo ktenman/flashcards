0186ad44e57c1aeb3061795c4dfedfa327e55cad
root <ktenman@users.noreply.github.com>
2017-01-09 18:32:17 +0200

    refresh

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!bin/bash
-docker cp $1:/src/db/cards.db `pwd`;
-docker stop $1 && docker rm $1;
+#docker cp cs-flash-cards:/src/db/cards.db `pwd`;
+docker stop cs-flash-cards && docker rm cs-flash-cards;
 docker rmi cs-flash-cards;
 docker build . -t cs-flash-cards;
-docker run -d -p 8000:8000 --name cs-flash-cards -v `pwd`:/src/db cs-flash-cards;
+docker run -d -p 80:8000 --name cs-flash-cards -v `pwd`:/src/db cs-flash-cards;
+
+docker cp teoreetiline:/src/db/cards.db `pwd`;
+docker stop teoreetiline && docker rm teoreetiline;
+docker rmi teoreetiline;
+docker build . -t teoreetiline;
+docker run -d -p 81:8000 --name teoreetiline -v `pwd`:/src/db teoreetiline;
+
 docker volume rm $(docker volume ls -qf dangling=true);
