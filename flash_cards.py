cc3cf921241f1db38d8750f8ca019c32595f05c4
Konstantin Tenman <ktenman@users.noreply.github.com>
2017-01-21 15:36:20 +0200

    refactoring

--- flash_cards.py
+++ flash_cards.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash
+
+from flask import Flask, request, session, g, redirect, url_for, render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
